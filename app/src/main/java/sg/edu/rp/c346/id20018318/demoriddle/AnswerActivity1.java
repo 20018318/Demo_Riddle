068b6141c72c80bfa75da253a69f525c5a451bfb
20018318 <20018318@myrp.edu.sg>
2021-05-24 10:21:42 +0800

    edit

--- app/src/main/java/sg/edu/rp/c346/id20018318/demoriddle/AnswerActivity1.java
+++ app/src/main/java/sg/edu/rp/c346/id20018318/demoriddle/AnswerActivity1.java
@@ -17,7 +17,6 @@ public class AnswerActivity1 extends AppCompatActivity {
         setContentView(R.layout.activity_answer1);
 
         tvAnswer = findViewById(R.id.textView);
-//        tvAnswer.setText("In Second Activity");
 
         Intent intentReceived = getIntent();
         String questionsSelected = intentReceived.getStringExtra("Question");
