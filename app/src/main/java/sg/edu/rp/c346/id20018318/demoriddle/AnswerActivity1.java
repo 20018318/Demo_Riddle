b81b681c7e4f3b5c3f81648b7003b809be805218
20018318 <20018318@myrp.edu.sg>
2021-05-24 14:52:10 +0800

    Android Activity Lifecycle

--- app/src/main/java/sg/edu/rp/c346/id20018318/demoriddle/AnswerActivity1.java
+++ app/src/main/java/sg/edu/rp/c346/id20018318/demoriddle/AnswerActivity1.java
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
 import android.widget.TextView;
 
@@ -15,6 +16,7 @@ public class AnswerActivity1 extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_answer1);
+        Log.d("AnswerActivity1", "onCreate() called.");
 
         tvAnswer = findViewById(R.id.textView);
 
@@ -26,5 +28,36 @@ public class AnswerActivity1 extends AppCompatActivity {
         } else if (questionsSelected.equalsIgnoreCase("q2")) {
             tvAnswer.setText(questionsSelected + " answer is: Gone");
         }
+    }
+
+    @Override
+    protected void onStart() {
+        Log.d("AnswerActivity1", "onStart() called.");
+        super.onStart();
+    }
+    @Override
+    protected void onResume() {
+        Log.d("AnswerActivity1", "onResume() called.");
+        super.onResume();
+    }
+    @Override
+    protected void onPause() {
+        Log.d("AnswerActivity1", "onPause() called.");
+        super.onPause();
+    }
+    @Override
+    protected void onStop() {
+        Log.d("AnswerActivity1", "onStop() called.");
+        super.onStop();
+    }
+    @Override
+    protected void onDestroy() {
+        Log.d("AnswerActivity1", "onDestroy() called.");
+        super.onDestroy();
+    }
+    @Override
+    protected void onRestart() {
+        Log.d("AnswerActivity1", "onRestart() called.");
+        super.onRestart();
     }
 }
